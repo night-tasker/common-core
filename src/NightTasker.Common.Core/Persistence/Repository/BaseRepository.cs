9788b4745db1d36b3bfddff208b4736e1ffa5355
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-12-16 22:09:34 +0300

    add repository abstracts

---
+++ src/NightTasker.Common.Core/Persistence/Repository/BaseRepository.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using NightTasker.Common.Core.Abstractions;
+
+namespace NightTasker.Common.Core.Persistence.Repository;
+
+/// <inheritdoc cref="IRepository{TEntity,TKey}"/>
+internal abstract class BaseRepository<TEntity, TKey, TDbContext>(TDbContext context) : IRepository<TEntity, TKey>
+    where TEntity : class, IEntity where TDbContext : DbContext
+{
+    /// <summary>
+    /// Записи определённой сущности.
+    /// </summary>
+    private DbSet<TEntity> Entities { get; init; } = context.Set<TEntity>();
+
+    /// <summary>
+    /// Таблица записей определённой сущности.
+    /// </summary>
+    public virtual IQueryable<TEntity> Table => Entities;
+
+    /// <summary>
+    /// Неотслеживаемая таблица определённой сущности.
+    /// </summary>
+    public virtual IQueryable<TEntity> NoTrackingTable => Entities.AsNoTrackingWithIdentityResolution();
+
+    /// <summary>
+    /// Получить все записи.
+    /// </summary>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Список всех записей.</returns>
+    public virtual async Task<List<TEntity>> GetAll(CancellationToken cancellationToken)
+    {
+        return await Entities.ToListAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Добавить новую запись.
+    /// </summary>
+    /// <param name="entity">Запись.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    public virtual async Task Add(TEntity entity, CancellationToken cancellationToken)
+    { 
+        await Entities.AddAsync(entity, cancellationToken);
+    }
+
+    /// <summary>
+    /// Обновить записи, удовлетворяющие условию.
+    /// </summary>
+    /// <param name="updateExpression">Условие.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    public virtual async Task UpdateByExpression(
+        Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> updateExpression, 
+        CancellationToken cancellationToken)
+    {
+        await Entities.ExecuteUpdateAsync(updateExpression, cancellationToken);
+    }
+
+    /// <summary>
+    /// Удалить записи, удовлетворяющие условию.
+    /// </summary>
+    /// <param name="deleteExpression">Условие.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    public virtual async Task DeleteByExpression(
+        Expression<Func<TEntity, bool>> deleteExpression,
+        CancellationToken cancellationToken)
+    {
+        await Entities.Where(deleteExpression).ExecuteDeleteAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Попробовать получить запись по ИД.
+    /// </summary>
+    /// <param name="entityId">ИД.</param>
+    /// <param name="cancellationToken">Токен.</param>
+    /// <returns>Запись.</returns>
+    public async Task<TEntity?> TryGetById(TKey entityId, CancellationToken cancellationToken)
+    {
+        var entity = await Entities.FindAsync(entityId);
+        return entity;
+    }
+}
---
+++ src/NightTasker.Common.Core/Persistence/Repository/IRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace NightTasker.Common.Core.Persistence.Repository;
+
+/// <summary>
+/// Репозиторий.
+/// </summary>
+/// <typeparam name="TEntity">Тип сущности.</typeparam>
+/// <typeparam name="TKey">Тип ключа сущности.</typeparam>
+public interface IRepository<TEntity, TKey>
+{
+    /// <summary>
+    /// Таблица записей определённой сущности.
+    /// </summary>
+    IQueryable<TEntity> Table { get; }
+    
+    /// <summary>
+    /// Неотслеживаемая таблица определённой сущности.
+    /// </summary>
+    IQueryable<TEntity> NoTrackingTable { get; }
+    
+    /// <summary>
+    /// Попробовать получить запись по ИД.
+    /// </summary>
+    /// <param name="entityId">ИД.</param>
+    /// <param name="cancellationToken">Токен.</param>
+    /// <returns>Запись.</returns>
+    Task<TEntity?> TryGetById(TKey entityId, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Получить все записи.
+    /// </summary>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Список всех записей.</returns>
+    Task<List<TEntity>> GetAll(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Добавить новую запись.
+    /// </summary>
+    /// <param name="entity">Запись.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    Task Add(TEntity entity, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Обновить записи, удовлетворяющие условию.
+    /// </summary>
+    /// <param name="updateExpression">Условие.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    Task UpdateByExpression(
+        Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> updateExpression,
+        CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// Удалить записи, удовлетворяющие условию.
+    /// </summary>
+    /// <param name="deleteExpression">Условие.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    Task DeleteByExpression(
+        Expression<Func<TEntity, bool>> deleteExpression,
+        CancellationToken cancellationToken);
+}
