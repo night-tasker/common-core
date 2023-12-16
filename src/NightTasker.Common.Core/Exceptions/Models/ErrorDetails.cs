fcaf667862d118fdfe66515151995869a45169dc
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-12-17 00:31:06 +0300

    add xml-documentation

--- src/NightTasker.Common.Core/Exceptions/Models/ErrorDetails.cs
+++ src/NightTasker.Common.Core/Exceptions/Models/ErrorDetails.cs
@@ -5,11 +5,23 @@
 /// </summary>
 public class ErrorDetails
 {
+    /// <summary>
+    /// Сообщение.
+    /// </summary>
     public string Message { get; set; } = null!;
 
+    /// <summary>
+    /// Сообщение для отображения.
+    /// </summary>
     public string? DisplayMessage { get; set; }
 
+    /// <summary>
+    /// Trace-ИД.
+    /// </summary>
     public string TraceId { get; set; } = null!;
 
+    /// <summary>
+    /// Стандартное сообщение ошибки.
+    /// </summary>
     public const string DefaultErrorMessage = "An error occurred while processing your request.";
 }
